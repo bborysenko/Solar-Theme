ce07824401f928bf42ee2f4e62b9dbbdaba6bd7c
Ricardo Rauch <ricardo@gravityonmars.com>
2012-09-08 12:03:40 -0300

    external url fix

--- inc/theme-options/theme-options.php
+++ inc/theme-options/theme-options.php
@@ -40,41 +40,66 @@ function theme_options_do_page() {
 				  	$color = '#ff0000';
 			?>
 
-				<h3><?php _e( 'Your name', 'wordpress-solar' ); ?></h3>
-				<input class="regular-text" type="text" name="solar_options[theme_username]" value="<?php esc_attr_e( $options['theme_username'] ); ?>" />
+			<table class="form-table">
+				<thead>
+					<tr>
+						<th><?php _e( 'Your name', 'wordpress-solar' ); ?></th>
+						<td>
+							<input class="regular-text" type="text" name="solar_options[theme_username]" value="<?php esc_attr_e( $options['theme_username'] ); ?>" />
+						</td>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<th><?php _e( 'Twitter Username', 'wordpress-solar' ); ?></th>
+						<td>
+							@<input class="regular-text" type="text" name="solar_options[twitter_username]" value="<?php esc_attr_e( $options['twitter_username'] ); ?>" />
+						</td>
+					</tr>
+					<tr>
+						<th><?php _e( 'Github Username', 'wordpress-solar' ); ?></th>
+						<td>
+							<input class="regular-text" type="text" name="solar_options[github_username]" value="<?php esc_attr_e( $options['github_username'] ); ?>" />
+						</td>
+					</tr>
 
+					<tr>
+						<th><?php _e( 'Contact Email', 'wordpress-solar' ); ?></th>
+						<td>
+							<input class="regular-text" type="text" name="solar_options[contact_email]" value="<?php esc_attr_e( $options['contact_email'] ); ?>" />
+						</td>
+					</tr>
 
-				<h3><?php _e( 'Twitter Username', 'wordpress-solar' ); ?></h3>
-				@<input class="regular-text" type="text" name="solar_options[twitter_username]" value="<?php esc_attr_e( $options['twitter_username'] ); ?>" />
-				<h3><?php _e( 'Github Username', 'wordpress-solar' ); ?></h3>
-				<input class="regular-text" type="text" name="solar_options[github_username]" value="<?php esc_attr_e( $options['github_username'] ); ?>" />
+					<tr>
+						<th><?php _e( 'Biography', 'wordpress-solar' ); ?></th>
+						<td>
+		<textarea id="solar_options[biography]" class="small-text" cols="50" rows="10" style="height: 100px;" name="solar_options[biography]"><?php echo esc_textarea( $options['biography'] ); ?></textarea>
+						</td>
+					</tr>
+
+					<tr>
+						<th><?php _e( 'Location', 'wordpress-solar' ); ?></th>
+						<td><input class="regular-text" type="text" name="solar_options[location]" value="<?php esc_attr_e( $options['location'] ); ?>" /></td>
+					</tr>
+
+					<tr>
+						<th><?php _e( 'Blog color', 'wordpress-solar' ); ?></th>
+						<td><div id="color_picker_color1"></div>
+					<input id="color1" class="regular-text" type="text" name="solar_options[color]" value="<?php echo $color; ?>" /></td>
+					</tr>
+
+					<tr>
+						<th><?php _e( 'Google Analytics // Typekit', 'wordpress-solar' ); ?></th>
+						<td><textarea id="solar_options[google_analytics]" class="small-text" cols="50" rows="10" name="solar_options[google_analytics]"><?php echo esc_textarea( $options['google_analytics'] ); ?></textarea></td>
+					</tr>
+				</tbody>
+			</table>
 				
-				<h3><?php _e( 'Contact Email', 'wordpress-solar' ); ?></h3>
-				<input class="regular-text" type="text" name="solar_options[contact_email]" value="<?php esc_attr_e( $options['contact_email'] ); ?>" />
-
-
-				<h3><?php _e( 'Biography', 'wordpress-solar' ); ?></h3>
-				<p>
-					<textarea id="solar_options[biography]" class="large-text" cols="30" rows="10" style="height: 100px; width: 400px" name="solar_options[biography]"><?php echo esc_textarea( $options['biography'] ); ?></textarea>
-				</p>
 			
-				<h3><?php _e( 'Location', 'wordpress-solar' ); ?></h3>
-				<input class="regular-text" type="text" name="solar_options[location]" value="<?php esc_attr_e( $options['location'] ); ?>" />
-				
-				<h3><?php _e( 'Blog color', 'wordpress-solar' ); ?></h3>
-				<p>
-					<div id="color_picker_color1"></div>
-					<input id="color1" class="regular-text" type="text" name="solar_options[color]" value="<?php echo $color; ?>" />
-				</p>
-				<h3><?php _e( 'Google Analytics // Typekit', 'wordpress-solar' ); ?></h3>
-				<p>
-					<textarea id="solar_options[google_analytics]" class="large-text" cols="50" rows="10" name="solar_options[google_analytics]"><?php echo esc_textarea( $options['google_analytics'] ); ?></textarea>
-				</p>
-			
-				<p class="submit">
-					<input type="submit" class="button-primary" value="<?php _e( 'Save Options', 'wordpress-solar' ); ?>" />
-				</p>
-	
+			<p class="submit">
+				<input type="submit" class="button-primary" value="<?php _e( 'Save Options', 'wordpress-solar' ); ?>" />
+			</p>
+
 		</form>
 	</div>
 	<?php
