782a1ca6d9583a6b5ad73a684e1cef19357510eb
Ricardo Rauch <ricardo@gravityonmars.com>
2012-09-08 11:53:47 -0300

    external url fix

--- content.php
+++ content.php
@@ -51,11 +51,5 @@
 			<?php endif; // End if $tags_list ?>
 		<?php endif; // End if 'post' == get_post_type() ?>
 
-		<?php if ( ! post_password_required() && ( comments_open() || '0' != get_comments_number() ) ) : ?>
-		<span class="sep"> | </span>
-		<span class="comments-link"><?php comments_popup_link( __( 'Leave a comment', 'solar' ), __( '1 Comment', 'solar' ), __( '% Comments', 'solar' ) ); ?></span>
-		<?php endif; ?>
-
-		<?php edit_post_link( __( 'Edit', 'solar' ), '<span class="sep"> | </span><span class="edit-link">', '</span>' ); ?>
 	</footer><!-- .entry-meta -->
 </article><!-- #post-<?php the_ID(); ?> -->
