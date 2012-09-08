9534d386c0cd0d5877388c540cf597f393b63fb1
Ricardo Rauch <ricardo@gravityonmars.com>
2012-09-08 11:20:20 -0300

    color fixes

--- page.php
+++ page.php
@@ -13,18 +13,16 @@
 
 get_header(); ?>
 
-<div id="primary" class="site-content">
-	<div id="content" role="main">
+<div id="content" role="main" class="content-blog">
 
-		<?php while ( have_posts() ) : the_post(); ?>
+	<?php while ( have_posts() ) : the_post(); ?>
 
-			<?php get_template_part( 'content', 'page' ); ?>
+		<?php get_template_part( 'content', 'page' ); ?>
 
-			<?php comments_template( '', true ); ?>
+		<?php comments_template( '', true ); ?>
 
-		<?php endwhile; // end of the loop. ?>
+	<?php endwhile; // end of the loop. ?>
 
-	</div><!-- #content -->
-</div><!-- #primary .site-content -->
+</div><!-- #content -->
 
 <?php get_footer(); ?>
